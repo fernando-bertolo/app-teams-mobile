5b83bca6c82997027447258cd7b2f9f7206b9358
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2024-09-21 16:14:34 -0300

    adicionando input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,16 @@
 import { TextInputProps } from 'react-native';
 import * as S from './styles'
+import { useTheme } from 'styled-components/native';
 
 
 export function Input({ ...rest }: TextInputProps) {
+  const { COLORS } = useTheme();
+
   return (
-    <S.Container {...rest}>
+    <S.Container 
+      {...rest}
+      placeholderTextColor={COLORS.GRAY_300}  
+    >
 
     </S.Container>
   );
